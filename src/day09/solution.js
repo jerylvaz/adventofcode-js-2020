f7e11f6f6343a09b313c6a07ff9a05c083a44cf2
Jeryl David Paul Vaz <Jeryl-David-Paul.Vaz@bmw.de>
2020-12-09 12:52:50 +0100

    Day 9

---
+++ src/day09/solution.js
@@ -0,0 +1,31 @@
+const name = 'Encoding Error';
+
+const preambleLen = 25;
+
+function combinations (arr) {
+    return arr.flatMap((val, idx) => arr.slice(idx + 1).map((x) => [val, x]));
+}
+
+function part1 (lines) {
+    return lines.map(Number)
+        .find((n, idx, arr) => idx >= preambleLen
+            && !combinations(arr.slice(idx - preambleLen, idx)).some(([x, y]) => x + y === n))
+        || -1;
+}
+
+function part2 (lines) {
+    const invalidNumber = part1(lines);
+    const nums = lines.map(Number);
+    for (let i = 0; i < nums.length - 2; ++i) {
+        for (let j = i + 2; j <= nums.length; ++j) {
+            const subArr = nums.slice(i, j);
+            if (subArr.reduce((acc, n) => acc + n, 0) === invalidNumber) {
+                const sortedSubArr = subArr.sort((a, b) => a - b);
+                return sortedSubArr[0] + sortedSubArr[sortedSubArr.length - 1];
+            }
+        }
+    }
+    return -1;
+}
+
+module.exports = { name, part1, part2 };
