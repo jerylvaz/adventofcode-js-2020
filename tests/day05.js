b32856d3e225c2e73b9e1bfd8000635c8340ff58
jerylvaz <jerylvaz@yahoo.com>
2020-12-10 11:59:22 +0100

    Add tests (#10)
    Co-authored-by: Jeryl David Paul Vaz <Jeryl-David-Paul.Vaz@bmw.de>

---
+++ tests/day05.js
@@ -0,0 +1,28 @@
+module.exports = {
+    part1: [
+        {
+            input: [
+                'FBFBBFFRLR'
+            ],
+            output: 357
+        },
+        {
+            input: [
+                'BFFFBBFRRR'
+            ],
+            output: 567
+        },
+        {
+            input: [
+                'FFFBBBFRRR'
+            ],
+            output: 119
+        },
+        {
+            input: [
+                'BBFFBBFRLL'
+            ],
+            output: 820
+        }
+    ]
+};
---
+++ tests/day06.js
@@ -0,0 +1,25 @@
+module.exports = {
+    both: [
+        {
+            input: [
+                'abc',
+                '',
+                'a',
+                'b',
+                'c',
+                '',
+                'ab',
+                'ac',
+                '',
+                'a',
+                'a',
+                'a',
+                'a',
+                '',
+                'b'
+            ],
+            output1: 11,
+            output2: 6
+        }
+    ]
+};
