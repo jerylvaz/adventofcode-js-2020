37ade62db8d6e07022cfadfcd00e35771396647f
Jeryl David Paul Vaz <Jeryl-David-Paul.Vaz@bmw.de>
2020-12-11 17:19:02 +0100

    Print run times

--- src/runner.js
+++ src/runner.js
@@ -17,21 +17,23 @@ function run (daysToRun = null) {
                     // eslint-disable-next-line import/no-dynamic-require, global-require
                     const solve = require(path.join(__dirname, dayDir, 'solution'));
                     return readInput(path.join(__dirname, dayDir, 'input.txt'))
-                        .then((data) => ([
-                            day,
-                            solve.name,
-                            solve.part1(data),
-                            solve.part2(data)
-                        ]));
+                        .then((data) => {
+                            const startTime = process.hrtime();
+                            const result1 = solve.part1(data);
+                            const result2 = solve.part2(data);
+                            const [, nanosecs] = process.hrtime(startTime);
+                            // eslint-disable-next-line no-bitwise
+                            return [day, solve.name, result1, result2, ~~(nanosecs / 1000000)];
+                        });
                 });
             return Promise.all(promises);
         })
         .then((results) => {
-            const heading = `${'DAY'.padStart(4)} | ${'NAME'.padEnd(24)} | ${'PART 1'.padStart(16)} | ${'PART 2'.padStart(16)}`;
+            const heading = `${'DAY'.padStart(4)} | ${'NAME'.padEnd(24)} | ${'PART 1'.padStart(16)} | ${'PART 2'.padStart(16)} | ${'TIME (ms)'.padStart(10)}`;
             const outputLines = [heading];
             outputLines.push('-'.repeat(heading.length));
-            results.forEach(([day, name, result1, result2]) => {
-                outputLines.push(`${`${day}`.padStart(4)} | ${name.padEnd(24)} | ${`${result1}`.padStart(16)} | ${`${result2}`.padStart(16)}`);
+            results.forEach(([day, name, result1, result2, time]) => {
+                outputLines.push(`${`${day}`.padStart(4)} | ${name.padEnd(24)} | ${`${result1}`.padStart(16)} | ${`${result2}`.padStart(16)} | ${`${time}`.padStart(10)}`);
             });
 
             return outputLines.join('\n');
