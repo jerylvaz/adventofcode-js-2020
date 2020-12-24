100a1aea71c0ebda9e2d2a197e3f16cc495f8e06
Jeryl David Paul Vaz <Jeryl-David-Paul.Vaz@bmw.de>
2020-12-24 20:56:41 +0100

    Day 24

---
+++ tests/day24.js
@@ -0,0 +1,30 @@
+module.exports = {
+    both: [
+        {
+            input: [
+                'sesenwnenenewseeswwswswwnenewsewsw',
+                'neeenesenwnwwswnenewnwwsewnenwseswesw',
+                'seswneswswsenwwnwse',
+                'nwnwneseeswswnenewneswwnewseswneseene',
+                'swweswneswnenwsewnwneneseenw',
+                'eesenwseswswnenwswnwnwsewwnwsene',
+                'sewnenenenesenwsewnenwwwse',
+                'wenwwweseeeweswwwnwwe',
+                'wsweesenenewnwwnwsenewsenwwsesesenwne',
+                'neeswseenwwswnwswswnw',
+                'nenwswwsewswnenenewsenwsenwnesesenew',
+                'enewnwewneswsewnwswenweswnenwsenwsw',
+                'sweneswneswneneenwnewenewwneswswnese',
+                'swwesenesewenwneswnwwneseswwne',
+                'enesenwswwswneneswsenwnewswseenwsese',
+                'wnwnesenesenenwwnenwsewesewsesesew',
+                'nenewswnwewswnenesenwnesewesw',
+                'eneswnwswnwsenenwnwnwwseeswneewsenese',
+                'neswnwewnwnwseenwseesewsenwsweewe',
+                'wseweeenwnesenwwwswnew'
+            ],
+            output1: 10,
+            output2: 2208
+        }
+    ]
+};
