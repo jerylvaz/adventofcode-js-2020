5668f823da2f77a5faa3b69b656486f4d12fb952
jerylvaz <jerylvaz@yahoo.com>
2020-12-27 17:46:50 +0100

    Day 23 (#33)
    Co-authored-by: Jeryl David Paul Vaz <Jeryl-David-Paul.Vaz@bmw.de>

---
+++ tests/day23.js
@@ -0,0 +1,11 @@
+module.exports = {
+    both: [
+        {
+            input: [
+                '389125467'
+            ],
+            output1: 67384529,
+            output2: 149245887792
+        }
+    ]
+};
