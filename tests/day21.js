b63b2945cb960963ccb6cb13081f0a950b31ca5e
jerylvaz <jerylvaz@yahoo.com>
2020-12-29 11:49:33 +0100

    Day 21 (#34)
    Co-authored-by: Jeryl David Paul Vaz <Jeryl-David-Paul.Vaz@bmw.de>

---
+++ tests/day21.js
@@ -0,0 +1,14 @@
+module.exports = {
+    both: [
+        {
+            input: [
+                'mxmxvkd kfcds sqjhc nhms (contains dairy, fish)',
+                'trh fvjkl sbzzf mxmxvkd (contains dairy)',
+                'sqjhc fvjkl (contains soy)',
+                'sqjhc mxmxvkd sbzzf (contains fish)'
+            ],
+            output1: 5,
+            output2: 'mxmxvkd,sqjhc,fvjkl'
+        }
+    ]
+};
